822b67cd78ffc42707e5b098eb6696239c706925
Isadora Yasmim <164579225+isadora-yasmim@users.noreply.github.com>
2025-01-28 22:07:03 -0300

    primeiros passos

---
+++ Exercícios-de-lógica/nível-básico/2.py
@@ -0,0 +1,6 @@
+nome = input('Digite seu nome:')
+print('Olá,', nome, 'prazer em te conhecer!')
+
+##outra maneira de fazer formatando o nome
+nomedois = input('Digite outro nome:')
+print('Olá, {} prazer em te conhecer!'.format(nomedois))
