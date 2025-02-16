426ea8d3a7d0f3fe17510459165c9d4e1a5a78bf
Isadora Yasmim <164579225+isadora-yasmim@users.noreply.github.com>
2025-02-16 17:12:40 -0300

    manipulando strings

---
+++ aulas/aula09.py
@@ -0,0 +1,18 @@
+frase='Curso em video python'
+print(frase[::2])
+print(frase.upper().count('O'))
+print(len(frase))
+print(frase.replace('python', 'android'))
+novafrase=frase.replace('python', 'android')
+print(frase)
+print(novafrase)
+dividido=frase.split()
+print(frase.split())
+print(dividido[0])
+print(dividido[0][3])
+
+
+
+
+#para imprimir um texto inteiro usar 3 aspas duplas
+#print("""Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.""")
